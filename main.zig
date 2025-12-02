65affef9e82165a5221347778149af9061cf925e
vrugtehagel <vrugtehagel@pm.me>
2025-12-02 17:06:05 +0100

    Formatting

--- main.zig
+++ main.zig
@@ -4,6 +4,9 @@ test {
     std.testing.refAllDecls(@This());
 }
 pub fn main() void {
-	_ = @import("day-1-part-1.zig");
-	_ = @import("day-1-part-2.zig");
+    _ = @import("day-1-part-1.zig");
+    _ = @import("day-1-part-2.zig");
+
+    _ = @import("day-2-part-1.zig");
+    _ = @import("day-2-part-2.zig");
 }
